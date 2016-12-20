b6714e0fab38b9b280011ca8facb6dc4f2977800
Lu, Sherwin <sherwin.lu@neustar.biz>
2016-12-20 02:55:47 -0800

    Fixed an issue with the Player AI always Hitting or Not Hitting on 16. The logic should be it depends on what the dealer is showing and only hit if they are showing a 17 or more, otherwise stay.

--- src/main/java/com/sherwin/Player.java
+++ src/main/java/com/sherwin/Player.java
@@ -8,8 +8,8 @@ import java.util.List;
  * Created by slu on 12/13/16.
  */
 public class Player {
-    public static final int DEFAULT_BET_AMT = 10;
-    private static final int INITIAL_CAPITAL = 200;
+    public static final int DEFAULT_BET_AMT = 1;
+    private static final int INITIAL_CAPITAL = Integer.MAX_VALUE / 10;
     private int bank = INITIAL_CAPITAL;
 
     private int wins = 0;
